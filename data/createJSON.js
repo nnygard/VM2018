db693da0b46125a4c3910f1593625cbb95bed48b
nygard001 <niklas.nygard@tre.se>
2018-06-13 18:10:19 +0200

    Commiting

--- data/createJSON.js
+++ data/createJSON.js
@@ -1,7 +1,8 @@
 var XLSX = require('XLSX');
 var jsonfile = require('jsonfile');
-var workbook = XLSX.readFile("VM_BetDraft.xlsx");
+var workbook = XLSX.readFile("VM_BetDraft.xlsx", {dateNF: 'yyyy-mm-dd'});
 var worksheet = workbook.Sheets["Sheet1"];
+XLSX.utils.format_cell(worksheet);
 var obj = XLSX.utils.sheet_to_json(worksheet);
 var jsonfile = require('jsonfile');
 var file = '../src/data.json';
--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
         super(props);
         this.state= {
             currentUser:false,
+            date:false,
         };
+        if (!this.state.date){
+            //Set DATE
+            this.state.date = new Date().toJSON().slice(0,10).replace(/-/g,'-');
+        }
     }
 
     setPlayer(name){
@@ -16,61 +21,119 @@ class App extends Component {
         });
     }
 
+    addDate(add){
+        var nextDate = new Date(this.state.date);
+        nextDate.setDate(nextDate.getDate()+add);
+        this.setState({
+            date: nextDate.toJSON().slice(0,10).replace(/-/g,'-'),
+        });
+    }
+
+    navigateToGames(){
+        //Todo: Navigate to closest Game
+        var FirstGame = new Date("2018-06-14").toJSON().slice(0,10).replace(/-/g,'-');
+        this.setState({
+            date: FirstGame,
+        });
+    }
+
     renderPlayer() {
         if (!this.state.currentUser){
             return (
                 <div>
-                    <h1>Välj En Spelare</h1>
+                <h1>Välj En Spelare</h1>
+                <p>{this.state.date}</p>
+                </div>
+            );
+        } else {
+            //If Games this date then one functions
+            //Else another function
+            return (
+                <div>
+
+                <div className="row">
+                    <span className="glyphicon glyphicon-chevron-up" aria-hidden="true" onClick={()=>this.addDate(-1)}></span>
+                </div>
+
+                <div className="row">
+                    <h2>{this.state.date}</h2>
+                </div>
+
+                {this.renderMatchesToday(this.state.date)}
+
+                <div className="row">
+                    <span className="glyphicon glyphicon-chevron-down" aria-hidden="true" onClick={()=>this.addDate(1)}></span>
+                </div>
+
                 </div>
             );
         }
 
+    }
+
+    renderMatchesToday(today){
+        var matchesFound = data.filter(
+            function(data) {
+                return data.Datum == today;
+            }
+        );
+
+        if (matchesFound!=""){
+            console.log(matchesFound);
+            return(
+
+                <div className="panel panel-primary">
+                <div className="panel-heading">
+                <h3 className="panel-title">Tyskland - Sverige, Kl 14:00</h3>
+                </div>
+                <div className="panel-body">
+                <p>2 - 1        1       67%</p>
+                </div>
+                </div>
+
+            );
+        }
         else {
             return (
                 <div>
-                    <h1>{this.state.currentUser}</h1>
-                    <p>Inga Matcher idag...</p>
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Tyskland - Sverige, Kl 14:00</h3>
-                        </div>
-                        <div class="panel-body">
-                            1, 2,1 Odds...
-                        </div>
-                    </div>
+                <div className="alert alert-danger" role="alert">
+                    <strong>Oh snap!</strong> Inga matcher idag.
+                </div>
+
+                <div className="row">
+                <p><button type="button" className="btn btn-lg btn-success" onClick={()=>this.navigateToGames()}>Gå till första matchen</button></p>
+                </div>
+
                 </div>
             );
         }
-
-
     }
 
     render() {
         return (
             <div>
-                <nav className="navbar navbar-default">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span className="sr-only">Toggle navigation</span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <a className="navbar-brand"  onClick={()=>this.setPlayer(false)}>VM2018</a>
-                        </div>
-                        <div className="navbar-collapse collapse">
-                            <ul className="nav navbar-nav">
-                                <li><a className="clickableName" onClick={()=>this.setPlayer("Niklas")}>Niklas</a></li>
-                                <li><a className="clickableName" onClick={()=>this.setPlayer("Patrik")}>Patrik</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div class="container">
-
-                    {this.renderPlayer()}
-                </div>
+            <nav className="navbar navbar-default">
+            <div className="container">
+            <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand"  onClick={()=>this.setPlayer(false)}>VM2018 {this.state.currentUser ? '- '+this.state.currentUser : 'Översikt'}</a>
+            </div>
+            <div className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+            <li className={this.state.currentUser === 'Niklas' ? 'active': ''}><a className="clickableName" onClick={()=>this.setPlayer("Niklas")}>Niklas</a></li>
+            <li className={this.state.currentUser === 'Patrik' ? 'active': ''}><a className="clickableName" onClick={()=>this.setPlayer("Patrik")}>Patrik</a></li>
+            </ul>
+            </div>
+            </div>
+            </nav>
+            <div className="container">
+            {this.renderPlayer()}
+            </div>
             </div>
         );
     }
